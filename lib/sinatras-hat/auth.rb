3a6820df33fd1b786c7c2e947a2ea958d54e0edc
Pat Nakajima <patnakajima@gmail.com>
2008-10-12 02:15:23 -0400

    corrected namespacing for auth module

--- lib/sinatras-hat/auth.rb
+++ lib/sinatras-hat/auth.rb
@@ -1,59 +1,61 @@
 # from http://www.gittr.com/index.php/archive/sinatra-basic-authentication-selectively-applied
 # adapted by pat nakajima for sinatra's hat
 module Sinatra
-  module Authorization
-    class ProtectedAction
-      attr_reader :klass, :context
+  module Hat
+    module Authorization
+      class ProtectedAction
+        attr_reader :klass, :context
       
-      def initialize(klass, context)
-        @klass, @context = klass, context
-      end
+        def initialize(klass, context)
+          @klass, @context = klass, context
+        end
       
-      def check!
-        unauthorized! unless auth.provided?
-        bad_request! unless auth.basic?
-        unauthorized! unless authorize(*auth.credentials)
-      end
+        def check!
+          unauthorized! unless auth.provided?
+          bad_request! unless auth.basic?
+          unauthorized! unless authorize(*auth.credentials)
+        end
       
-      def username
-        auth.username
-      end
+        def username
+          auth.username
+        end
       
-      private
+        private
       
-      def authorize(username, password)
-        klass.credentials[:username] == username and klass.credentials[:password] == password
-      end
+        def authorize(username, password)
+          klass.credentials[:username] == username and klass.credentials[:password] == password
+        end
       
-      def unauthorized!(realm="myApp.com")
-        context.header 'WWW-Authenticate' => %(Basic realm="#{klass.realm}")
-        throw :halt, [ 401, 'Authorization Required' ]
-      end
+        def unauthorized!(realm="myApp.com")
+          context.header 'WWW-Authenticate' => %(Basic realm="#{klass.realm}")
+          throw :halt, [ 401, 'Authorization Required' ]
+        end
 
-      def bad_request!
-        throw :halt, [ 400, 'Bad Request' ]
-      end
+        def bad_request!
+          throw :halt, [ 400, 'Bad Request' ]
+        end
       
-      def auth
-        @auth ||= Rack::Auth::Basic::Request.new(context.request.env)
+        def auth
+          @auth ||= Rack::Auth::Basic::Request.new(context.request.env)
+        end
       end
-    end
     
-    module Helpers
-      def protect!(klass)
-        return if authorized?
-        guard = ProtectedAction.new(klass, self)
-        guard.check!
-        request.env['REMOTE_USER'] = guard.username
-      end
+      module Helpers
+        def protect!(klass)
+          return if authorized?
+          guard = ProtectedAction.new(klass, self)
+          guard.check!
+          request.env['REMOTE_USER'] = guard.username
+        end
 
-      def authorized?
-        request.env['REMOTE_USER']
+        def authorized?
+          request.env['REMOTE_USER']
+        end
       end
     end
   end
 end
 
 helpers do
-  include Sinatra::Authorization::Helpers
+  include Sinatra::Hat::Authorization::Helpers
 end
