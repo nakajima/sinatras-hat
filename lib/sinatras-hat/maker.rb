96eeecbb25c93795fd3213880b808f15d5011a27
nakajima <patnakajima@gmail.com>
2009-01-08 23:44:40 -0700

    owning up to it

--- lib/sinatras-hat/maker.rb
+++ lib/sinatras-hat/maker.rb
@@ -10,6 +10,7 @@ module Sinatra
         @actions ||= { }
       end
       
+      # enables the douche-y DSL you see in actions.rb
       def self.action(name, path, options={}, &block)
         verb = options[:verb] || :get
         Router.cache << [verb, name, path]
