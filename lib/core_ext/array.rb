5171f62160f36a36139e2fc5ba547c36852fe018
Pat Nakajima <patnakajima@gmail.com>
2009-01-12 19:55:05 -0500

    removed some un-used core_ext methods

--- lib/core_ext/array.rb
+++ lib/core_ext/array.rb
@@ -2,16 +2,4 @@ class Array
   def extract_options!
     last.is_a?(Hash) ? pop : { }
   end
-  
-  def move_to_front(*entries)
-    entries.each do |entry|
-      unshift(entry) if delete(entry)
-    end
-  end
-  
-  def move_to_back(*entries)
-    entries.each do |entry|
-      push(entry) if delete(entry)
-    end
-  end
 end
