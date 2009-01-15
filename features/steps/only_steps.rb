f6973d6eb617bca34c2c8001bdc65c8a6f06e084
Pat Nakajima <patnakajima@gmail.com>
2009-01-14 23:40:39 -0500

    removed unnecessary logging

--- features/steps/only_steps.rb
+++ features/steps/only_steps.rb
@@ -1,7 +1,7 @@
 Given /^I mount the model for only the :index action$/ do
   @app = mock_app do
     set :views, File.join(File.dirname(__FILE__), '..', 'support', 'views')
-    set :logging, true
+    # set :logging, true
     
     mount Person do
       only :index
