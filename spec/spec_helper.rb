a4964b3da4878fea51a57eb3abcba6e8e2166fb8
Pat Nakajima <patnakajima@gmail.com>
2009-01-26 22:46:45 -0500

    added mock_app helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,15 @@ build_model(:comments) do
   belongs_to :article
 end
 
+module Sinatra::Test
+  # Sets up a Sinatra::Base subclass defined with the block
+  # given. Used in setup or individual spec methods to establish
+  # the application.
+  def mock_app(base=Sinatra::Base, &block)
+    @app = Sinatra.new(base, &block)
+  end
+end
+
 def build_models!
   Article.delete_all
   Comment.delete_all
